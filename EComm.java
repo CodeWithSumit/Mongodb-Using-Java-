1b779ae27251c0ff14749a45893e28835451ad47
Sumit Kumar Singh <sumit1412@hotmail.com>
2015-07-24 02:13:45 +0530

    Update EComm.java

--- EComm.java
+++ EComm.java
@@ -3,13 +3,14 @@ package com.eCommerce.database.com;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.UnknownHostException;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
-public class ecommerceDB {
+public class EcommerceDB {
 	public static void main(String[] args) throws IOException{
 		MongoClient mongoclient = new MongoClient("localhost",27017);
 			DB db = mongoclient.getDB("E-Commerce");
@@ -28,7 +29,7 @@ public class ecommerceDB {
          String pwd = bufferedreader.readLine();
          if(db.authenticate(user, pwd.toCharArray()))
          {   
-        	 System.out.println("Which Product you want to insert");
+        	 System.out.println("Which Product Category you want to insert");
               String ProdCategory =null;
               ProdCategory = bufferedreader.readLine();
               if(ProdCategory.equals("Clothes")) 
@@ -46,6 +47,11 @@ public class ecommerceDB {
             	  DBCollection Crockerycollection =db.getCollection("Crockery");
             	  crockery(bufferedreader,Crockerycollection);
               }
+              else if (ProdCategory.equals("Electronics Gadgets"))
+              {
+            	  DBCollection Crockerycollection =db.getCollection("Electronics Gadgets");
+            	  Electronics(bufferedreader,Crockerycollection);
+              }
         	 String Command=null;
         	 while(true)
         	 {
@@ -53,6 +59,13 @@ public class ecommerceDB {
         	 }
         	
          }
+         return flag;
+	}
+
+	private static void Electronics(BufferedReader bufferedreader,
+			DBCollection crockerycollection) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static void crockery(BufferedReader bufferedreader,
@@ -68,9 +81,50 @@ public class ecommerceDB {
 	}
 
 	private static void clothes(BufferedReader bufferedreader,
-			DBCollection Clothescollection) {
+			DBCollection Clothescollection) throws IOException {
+		    String action = null;
+	        System.out.println("Choose Your Action");
+	        System.out.println("Insert");
+	        System.out.println("Update");
+	        System.out.println("Find");
+	        System.out.println("Demand a Clothe");
+	        action = bufferedreader.readLine();
+	        if(action.equals("Insert")) InsertClothes(bufferedreader,Clothescollection);
+	        	
+	        	
+	        
+	        
+	        
 		
 		
+	}
+
+	private static void InsertClothes(BufferedReader bufferedreader,
+			DBCollection clothescollection) {
+		BasicDBList dbl = new BasicDBList();
+		BasicDBList db2 = new BasicDBList();
+		BasicDBList db3 = new BasicDBList();
+		dbl.add(new BasicDBObject("Color","Blue").append("Type", "Skinny Fit").append("Price", 1500));
+		db2.add(new BasicDBObject("Color","Purple").append("Type", "Comfortable Fit").append("Price", 2000));
+		db3.add(new BasicDBObject("Color","Black").append("Type", "Check").append("Price", 1000));
+		//dbl.add(new BasicDBObject("Type", "Skinny Fit"));
+		//dbl.add(new BasicDBObject("Price", 1500));
+		//db2.add(new BasicDBObject("Color","Purple"));
+		//db2.add(new BasicDBObject("Type", "Comfortable Fit"));
+		//db2.add(new BasicDBObject("Price", 2000));
+		//db3.add(new BasicDBObject("Color","Black"));
+		//db3.add(new BasicDBObject("Type", "Checks"));
+		//db3.add(new BasicDBObject("Price", 1000 ));
+		db3.add(new BasicDBObject("Color","Red").append("Type", "Slim fit").append("Price", 1200));
+		//db3.add(new BasicDBObject("Type", "Slim fit"));
+		//db3.add(new BasicDBObject("Price", 1200));
+		BasicDBObject MenObject =new BasicDBObject("Values", "Men")
+	    .append("Jeans", dbl)
+	    .append("Trouser", db2)
+		.append("Shirt", db3);
+		clothescollection.insert(MenObject);
+		
+	
 	}
 
 }
