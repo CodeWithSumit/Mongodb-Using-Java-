6bbd7cb17b03b7b3c991c5de8d8d316d8c299613
Sumit Kumar Singh <sumit1412@hotmail.com>
2016-03-10 22:17:28 +0530

    Update Conversion.sql

--- Conversion.sql
+++ Conversion.sql
@@ -1,139 +1,41 @@
-SELECT
-		 ID.LAB_NAME
-		,ID.LAB_ACCESSION_NR
-		,ID.LAB_SPECIMEN_NUM
-		,ID.FIELD_SPECIMEN_NUM
-		--REGEXP_SUBSTR(ID.FIELD_SPECIMEN_NUM, '[A][[:digit:]]{6}') AS "Barcode",
-		,REPLACE(CONVERT(VARCHAR(9), PCR.TEST_DATE, 6), ' ', '-') AS MTX_PCR_TEST_DATE
-		,PCR.TEST_INTERP AS MTX_PCR_RESULT
-		,REPLACE(CONVERT(VARCHAR(9), H5.TEST_DATE, 6), ' ', '-') AS H5_TEST_DATE
-		,H5.TEST_INTERP AS H5_RESULT
-		,REPLACE(CONVERT(VARCHAR(9), H7.TEST_DATE, 6), ' ', '-') AS H7_TEST_DATE
-		,H7.TEST_INTERP AS H7_RESULT
-		,REPLACE(CONVERT(VARCHAR(9), VI.TEST_DATE, 6), ' ', '-') AS VI_TEST_DATE
-		,VI.TEST_INTERP AS VI_RESULT
-FROM
-( (
-SELECT 
-		 LAB_NAME
-		,LAB_ACCESSION_NR 
-		,LAB_SPECIMEN_NUM
-		,FIELD_SPECIMEN_NUM
-		,SPEC_TYPE_CODE
-		,TEST_DATE
-		,TEST_INTERP
-		,DATE_SPEC_RECVD
-FROM
-( 
-SELECT 
-		 DATE_SPEC_RECVD
-		,MSG_DATE_RECVD
-		,LAB_NAME
-		,LAB_ACCESSION_NR
-		,FIELD_SPECIMEN_NUM
-		,LAB_SPECIMEN_NUM
-		,SPEC_TYPE_CODE
-		,TEST_DATE
-		,TEST_INTERP
-		,ROW_NUMBER() OVER (PARTITION BY LAB_NAME, LAB_ACCESSION_NR, LAB_SPECIMEN_NUM, FIELD_SPECIMEN_NUM, SPEC_TYPE_CODE 
-		ORDER BY TEST_DATE DESC, 
-		MSG_DATE_RECVD DESC, 
-		(CASE
-			WHEN TEST_INTERP LIKE '%POS%' THEN 1
-			ELSE 2
-		END) ASC
-		) AS TEST_DATE_ORDER
-FROM LMS.TEST_GROUP_DETAIL_STG
-WHERE TEST_GROUP = 'VSAI'
---AND PROGRAM_OID = '2.16.840.1.113883.3.5.8.4.2' --AI LMB SURVEILLANCE
-AND TEST_LOINC IN ('44263-2','44264-0','44266-5','X0040-6', '48310-7')
-AND DELETED IS NULL
-AND TEST_INTERP <> 'X'
-AND MSG_DATE_RECVD BETWEEN START_DATE AND END_DATE   --I am not sure what is this Start_date and End_date it must be a hardcode value or variable like @Start_date and @End_date
-ORDER BY LAB_NAME, LAB_ACCESSION_NR, LAB_SPECIMEN_NUM ASC, FIELD_SPECIMEN_NUM ASC, TEST_LOINC
-) ID
-LEFT JOIN 
-(
-SELECT 
-	 LAB_NAME
-	,LAB_ACCESSION_NR 
-	,LAB_SPECIMEN_NUM 
-	,FIELD_SPECIMEN_NUM 
-	,SPEC_TYPE_CODE
-	,TEST_DATE
-	,TEST_INTERP
-FROM
-
-(
-
-SELECT 
-	 DATE_SPEC_RECVD
-	,MSG_DATE_RECVD
-	,LAB_NAME
-	,LAB_ACCESSION_NR
-	,FIELD_SPECIMEN_NUM
-	,ANIMAL_ID
-	,LAB_SPECIMEN_NUM
-	,SPEC_TYPE_CODE
-	,TEST_DATE
-	,TEST_INTERP
-	,ROW_NUMBER() OVER (PARTITION BY LAB_NAME, LAB_ACCESSION_NR, LAB_SPECIMEN_NUM, FIELD_SPECIMEN_NUM, SPEC_TYPE_CODE 
-	ORDER BY TEST_DATE DESC, 
-	MSG_DATE_RECVD DESC, 
-	(CASE
-	WHEN TEST_INTERP LIKE '%POS%' THEN 1
-	ELSE 2
-	END) ASC
-	) AS TEST_DATE_ORDER
-FROM LMS.TEST_GROUP_DETAIL_STG
-WHERE TEST_GROUP = 'VSAI'
---AND PROGRAM_OID = '2.16.840.1.113883.3.5.8.4.2' --AI LMB SURVEILLANCE
-AND TEST_LOINC = '44263-2'
-AND DELETED IS NULL
-AND TEST_INTERP <> 'X'
-AND MSG_DATE_RECVD BETWEEN START_DATE AND END_DATE  --I am not sure what is this Start_date and End_date it must be a hardcode value or variable like @Start_date and @End_date
-ORDER BY LAB_NAME, LAB_ACCESSION_NR, LAB_SPECIMEN_NUM ASC, FIELD_SPECIMEN_NUM ASC, TEST_LOINC 
-) PCR_INNER
+Select Lab_Name,Lab_Accession_NR,LabSpecimenNumber,FieldSpecimenNumber,SpecTypeCode,TestDate,TestInterp
+(Select 
+      CRMAF_lms_lms_result.lms_spm_spm_18 DATE_SPEC_RECVD
+	 ,CRMAF_lms_lms_result._____________  MSG_DATE_RECVD ----Don't find this field in the mapping query given
+	 ,lab.lms_symbol Lab_Name
+	 ,lab.lms_pv1_pv1_19_cx_1 Lab_Accession_NR        --- Just an assumption that data coming from same table as Lab Name
+	 ,CRMAF_lms_lms_result.lms_spm_spm_2_eip_1_ei_1 FieldSpecimenNumber
+	 ,pid3.lms_pid_pid3_cx1 AnimalID
+	 ,CRMAF_lms_lms_result.lms_spm_spm_2_eip_2_ei_1 LabSpecimenNumber
+	 ,CRMAF_lms_lms_result.lms_spm_spm_4_cwe_1 SpecTypeCode
+	 ,CRMAF_lms_lms_result.lms_obx_obx_19 TestDate
+	 ,CRMAF_lms_lms_result.lms_obx_obx_8 TestInterp
+	 ,ROW_NUMBER() OVER (PARTITION BY lab.lms_symbol, lab.lms_pv1_pv1_19_cx_1, CRMAF_lms_lms_result.lms_spm_spm_2_eip_2_ei_1, CRMAF_lms_lms_result.lms_spm_spm_2_eip_1_ei_1, CRMAF_lms_lms_result.lms_spm_spm_4_cwe_1
+	  ORDER BY CRMAF_lms_lms_result.lms_obx_obx_19 DESC,
+	  CRMAF_lms_lms_result._____________ DESC,
+	  (CASE
+	  WHEN CRMAF_lms_lms_result.lms_obx_obx_8 LIKE '%POS%' THEN 1
+	  ELSE 2
+	  END) ASC
+	  ) AS TEST_DATE_ORDER
+     From Filteredlms_lms_result AS CRMAF_lms_lms_result
+	 Left Join Filteredlms_lab lab on CRMAF_lms_lms_result.lms_msh_msh_4_hd_1 = lab.lms_nais
+	 left join Filteredlms_pid_pid3 pid3 on CRMAF_lms_lms_result.lms_patientid = pid3.lms_pid_pid3id
+	 WHERE TEST_GROUP = 'VSAI'
+	--AND PROGRAM_OID = '2.16.840.1.113883.3.5.8.4.2' --AI LMB SURVEILLANCE
+	AND CRMAF_lms_lms_result.lms_obx_obx_3_cwe_1 = '44263-2'
+	AND _______________ DELETED IS NULL   -- Don't know where this deleted column is coming from
+	AND CRMAF_lms_lms_result.lms_obx_obx_8 <> 'X'
+	AND CRMAF_lms_lms_result._____________ BETWEEN :START_DATE AND :END_DATE
+	ORDER BY lab.lms_symbol, 
+	lab.lms_pv1_pv1_19_cx_1, 
+	CRMAF_lms_lms_result.lms_spm_spm_2_eip_2_ei_1 ASC, 
+	CRMAF_lms_lms_result.lms_spm_spm_2_eip_1_ei_1 ASC, 
+	CRMAF_lms_lms_result.lms_obx_obx_3_cwe_1
+	) PCR_INNER
 WHERE TEST_DATE_ORDER = '1'
-GROUP BY LAB_NAME, LAB_ACCESSION_NR, LAB_SPECIMEN_NUM, FIELD_SPECIMEN_NUM, SPEC_TYPE_CODE, TEST_DATE, TEST_INTERP
+GROUP BY Lab_Name, Lab_Accession_NR, LabSpecimenNumber, FieldSpecimenNumber, SpecTypeCode, TestDate, TestInterp
 ) PCR
-ON (ID.LAB_NAME = PCR.LAB_NAME AND
-ID.LAB_ACCESSION_NR = PCR.LAB_ACCESSION_NR AND
-ID.LAB_SPECIMEN_NUM = PCR.LAB_SPECIMEN_NUM AND
-ID.FIELD_SPECIMEN_NUM = PCR.FIELD_SPECIMEN_NUM AND
-ID.SPEC_TYPE_CODE = PCR.SPEC_TYPE_CODE)
 
-LEFT JOIN
-(
-SELECT 
-	 LAB_NAME
-	,LAB_ACCESSION_NR
-	,LAB_SPECIMEN_NUM
-	,FIELD_SPECIMEN_NUM
-	,SPEC_TYPE_CODE
-	,TEST_DATE
-	,TEST_INTERP
-FROM
-(
-
-SELECT 
-	 DATE_SPEC_RECVD
-	,MSG_DATE_RECVD
-	,LAB_NAME
-	,LAB_ACCESSION_NR
-	,LAB_SPECIMEN_NUM
-	,FIELD_SPECIMEN_NUM
-	,SPEC_TYPE_CODE
-	,TEST_DATE,TEST_INTERP
-	,ROW_NUMBER() OVER (PARTITION BY LAB_NAME, LAB_ACCESSION_NR, LAB_SPECIMEN_NUM, FIELD_SPECIMEN_NUM, SPEC_TYPE_CODE 
-	ORDER BY TEST_DATE DESC, 
-	MSG_DATE_RECVD DESC, 
-	(CASE
-	WHEN TEST_INTERP LIKE '%POS%' THEN 1
-	ELSE 2
-	END)                                          
-/* I have converted this query, 
-   I think this query is incomplete at the third level 
-   and this will work fine on sql server */
 
 
